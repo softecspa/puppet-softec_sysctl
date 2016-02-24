51e83a3370c6199c848cee293fea5645ca77dedf
lorello <lorenzo.salvadorini@softecspa.it>
2016-02-24 11:09:26 +0100

    Switched from our custom sysctl module to herculesteam/augeasproviders_sysctl

--- manifests/aws.pp
+++ manifests/aws.pp
@@ -1,25 +1,26 @@
+# Simple sysctl settings for VM on AWS
 class softec_sysctl::aws {
 
   $shortname = inline_template("<%= clientcert.split('.').at(0) %>")
 
-  sysctl::conf {'net_ipv4_ip_local_port_range':
-    key   => 'net.ipv4.ip_local_port_range',
-    value => '1024 65535',
+  sysctl { 'net.ipv4.ip_local_port_range':
+    ensure => present,
+    value  => '1024 65535',
   }
 
-  sysctl::conf {'net_ipv4_tcp_max_syn_backlog':
-    key   => 'net.ipv4.tcp_max_syn_backlog',
-    value => '4096',
+  sysctl { 'net.ipv4.tcp_max_syn_backlog':
+    ensure => present,
+    value  => '4096',
   }
 
-  sysctl::conf {'net_ipv4_tcp_fin_timeout':
-    key   => 'net.ipv4.tcp_fin_timeout',
-    value => '30',
+  sysctl { 'net.ipv4.tcp_fin_timeout':
+    ensure => present,
+    value  => '30',
   }
 
-  sysctl::conf {'kernel_hostname':
-    key   =>'kernel.hostname',
-    value => $shortname,
+  sysctl { 'kernel.hostname':
+    ensure => present,
+    value  => $shortname,
   }
 
 }
